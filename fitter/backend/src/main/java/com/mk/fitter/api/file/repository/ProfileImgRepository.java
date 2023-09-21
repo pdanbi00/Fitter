fef29459b03390612b3fb8f1c5088b05627c6c0d
김경숙 <lovablekks@naver.com>
2023-09-21 17:57:00 +0900

    Docs: 한 일 업로드

--- fitter/backend/src/main/java/com/mk/fitter/api/file/repository/ProfileImgRepository.java
+++ fitter/backend/src/main/java/com/mk/fitter/api/file/repository/ProfileImgRepository.java
@@ -1,4 +1,11 @@
 package com.mk.fitter.api.file.repository;
 
-public interface ProfileImgRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mk.fitter.api.file.repository.dto.ProfileImgDto;
+
+@Repository
+public interface ProfileImgRepository extends JpaRepository<ProfileImgDto, Integer> {
+
 }
