31963bd2693c89ec43958074c9484bfd19a41b8a
김경숙 <lovablekks@naver.com>
2023-09-22 11:12:56 +0900

    Feat: 사용자 프로필사진 API 구현

--- fitter/backend/src/main/java/com/mk/fitter/api/file/service/FileService.java
+++ fitter/backend/src/main/java/com/mk/fitter/api/file/service/FileService.java
@@ -1,4 +1,13 @@
 package com.mk.fitter.api.file.service;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import com.mk.fitter.api.file.repository.dto.ProfileImgDto;
+
 public interface FileService {
+	ProfileImgDto saveProfileImg(MultipartFile file) throws Exception;
+
+	void deleteProfileImg(ProfileImgDto profile) throws Exception;
+
+	String createRandomFileName() throws Exception;
 }
