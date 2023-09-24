825f443a7cc5cd454ed7eadf84cb9547aabec571
최영창 <choiyc1446@gmail.com>
2023-09-24 21:21:23 +0900

    Refactor: 코드 컨벤션 준수

--- fitter/backend/src/main/java/com/mk/fitter/api/personalrecord/controller/PersonalRecordController.java
+++ fitter/backend/src/main/java/com/mk/fitter/api/personalrecord/controller/PersonalRecordController.java
@@ -1,25 +1,34 @@
 package com.mk.fitter.api.personalrecord.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import com.mk.fitter.api.common.service.JwtService;
 import com.mk.fitter.api.personalrecord.repository.dto.PersonalRecordCreateRequest;
 import com.mk.fitter.api.personalrecord.repository.dto.PersonalRecordDto;
 import com.mk.fitter.api.personalrecord.repository.dto.WorkoutDto;
 import com.mk.fitter.api.personalrecord.repository.dto.WorkoutTypeDto;
 import com.mk.fitter.api.personalrecord.service.PersonalRecordService;
+
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiModelProperty;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -28,135 +37,134 @@ import java.util.Optional;
 @Slf4j
 public class PersonalRecordController {
 
-    private final PersonalRecordService personalRecordService;
-    private final JwtService jwtService;
+	private final PersonalRecordService personalRecordService;
+	private final JwtService jwtService;
 
-    @GetMapping("/category")
-    @ApiOperation(value = "운동 대분류 리스트", notes = "운동 대분류 리스트 불러오는 API")
-    public ResponseEntity<List<WorkoutTypeDto>> getWorkoutCategory() {
-        try {
-            List<WorkoutTypeDto> category = personalRecordService.getWorkoutCategory();
-            return new ResponseEntity<>(category, HttpStatus.OK);
-        } catch (Exception e) {
-            log.error(e.getMessage());
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
+	@GetMapping("/category")
+	@ApiOperation(value = "운동 대분류 리스트", notes = "운동 대분류 리스트 불러오는 API")
+	public ResponseEntity<List<WorkoutTypeDto>> getWorkoutCategory() {
+		try {
+			List<WorkoutTypeDto> category = personalRecordService.getWorkoutCategory();
+			return new ResponseEntity<>(category, HttpStatus.OK);
+		} catch (Exception e) {
+			log.error(e.getMessage());
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 
-    @GetMapping("/list/{workoutName}")
-    @ApiOperation(value = "해당 운동의 모든 기록", notes = "해당 운동의 모든 기록들을 불러오는 API")
-    public ResponseEntity<List<PersonalRecordDto>> getRecordListByWorkoutName(@RequestHeader String Authorization,
-                                                                              @PathVariable String workoutName) {
-        try {
-            Optional<Integer> UID = jwtService.extractUID(Authorization);
-            List<PersonalRecordDto> result = personalRecordService.getRecordListByWorkoutName(UID.get(), workoutName);
-            return new ResponseEntity<>(result, HttpStatus.OK);
-        } catch (Exception e) {
-            log.error(e.getMessage());
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
+	@GetMapping("/list/{workoutName}")
+	@ApiOperation(value = "해당 운동의 모든 기록", notes = "해당 운동의 모든 기록들을 불러오는 API")
+	public ResponseEntity<List<PersonalRecordDto>> getRecordListByWorkoutName(@RequestHeader String Authorization,
+		@PathVariable String workoutName) {
+		try {
+			Optional<Integer> UID = jwtService.extractUID(Authorization);
+			List<PersonalRecordDto> result = personalRecordService.getRecordListByWorkoutName(UID.get(), workoutName);
+			return new ResponseEntity<>(result, HttpStatus.OK);
+		} catch (Exception e) {
+			log.error(e.getMessage());
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 
-    @GetMapping("/list/rank")
-    @ApiOperation(value = "각 운동별 최고 기록", notes = "각 운동별 최고 기록을 리스트로 제공하는 API")
-    public ResponseEntity<List<Map<String, String>>> getRankList(@RequestHeader String Authorization) {
-        try {
-            Optional<Integer> UID = jwtService.extractUID(Authorization);
-            return new ResponseEntity<>(personalRecordService.getRankList(UID.get()), HttpStatus.OK);
-        } catch (Exception e) {
-            log.error(e.getMessage());
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+	@GetMapping("/list/rank")
+	@ApiOperation(value = "각 운동별 최고 기록", notes = "각 운동별 최고 기록을 리스트로 제공하는 API")
+	public ResponseEntity<List<Map<String, String>>> getRankList(@RequestHeader String Authorization) {
+		try {
+			Optional<Integer> UID = jwtService.extractUID(Authorization);
+			return new ResponseEntity<>(personalRecordService.getRankList(UID.get()), HttpStatus.OK);
+		} catch (Exception e) {
+			log.error(e.getMessage());
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 
-    }
+	@GetMapping("/list/rank/test")
+	@ApiOperation(value = "각 운동별 최고 기록", notes = "각 운동별 최고 기록을 리스트로 제공하는 API")
+	public ResponseEntity<List<Map<String, String>>> getRankListTest() {
+		try {
+			return new ResponseEntity<>(personalRecordService.getRankList(28), HttpStatus.OK);
+		} catch (Exception e) {
+			log.error(e.getMessage());
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 
-    @GetMapping("/list/rank/test")
-    @ApiOperation(value = "각 운동별 최고 기록", notes = "각 운동별 최고 기록을 리스트로 제공하는 API")
-    public ResponseEntity<List<Map<String, String>>> getRankListTest() {
-        try {
-            return new ResponseEntity<>(personalRecordService.getRankList(28), HttpStatus.OK);
-        } catch (Exception e) {
-            log.error(e.getMessage());
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+	}
 
-    }
+	@GetMapping("/workout")
+	@ApiOperation(value = "운동 목록 조회", notes = "운동 목록 리스트를 조회하는 API")
+	public ResponseEntity<List<WorkoutDto>> getWorkoutList() {
+		try {
+			return new ResponseEntity<>(personalRecordService.getWorkoutList(), HttpStatus.OK);
+		} catch (Exception e) {
+			log.error(e.getMessage());
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 
-    @GetMapping("/workout")
-    @ApiOperation(value = "운동 목록 조회", notes = "운동 목록 리스트를 조회하는 API")
-    public ResponseEntity<List<WorkoutDto>> getWorkoutList() {
-        try {
-            return new ResponseEntity<>(personalRecordService.getWorkoutList(), HttpStatus.OK);
-        } catch (Exception e) {
-            log.error(e.getMessage());
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
+	@GetMapping("/list")
+	@ApiOperation(value = "1RM 기록 리스트", notes = "개인의 1RM리스트를 조회하는 API")
+	public ResponseEntity<List<PersonalRecordDto>> getRecordList(@RequestHeader String Authorization) {
+		try {
+			Optional<Integer> UID = jwtService.extractUID(Authorization);
+			List<PersonalRecordDto> result = personalRecordService.getRecordList(UID.get());
+			return new ResponseEntity<>(result, HttpStatus.OK);
+		} catch (Exception e) {
+			log.error(e.getMessage());
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 
-    @GetMapping("/list")
-    @ApiOperation(value = "1RM 기록 리스트", notes = "개인의 1RM리스트를 조회하는 API")
-    public ResponseEntity<List<PersonalRecordDto>> getRecordList(@RequestHeader String Authorization) {
-        try {
-            Optional<Integer> UID = jwtService.extractUID(Authorization);
-            List<PersonalRecordDto> result = personalRecordService.getRecordList(UID.get());
-            return new ResponseEntity<>(result, HttpStatus.OK);
-        } catch (Exception e) {
-            log.error(e.getMessage());
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
+	@GetMapping("/read/{personalRecordId}")
+	@ApiOperation(value = "1RM 단일 기록 조회", notes = "1RM 기록의 세부사항을 조회하는 API")
+	public ResponseEntity<PersonalRecordDto> getRecord(@PathVariable int personalRecordId) {
+		try {
+			PersonalRecordDto result = personalRecordService.getRecord(personalRecordId);
+			return new ResponseEntity<>(result, HttpStatus.OK);
+		} catch (Exception e) {
+			log.error(e.getMessage());
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 
-    @GetMapping("/read/{personalRecordId}")
-    @ApiOperation(value = "1RM 단일 기록 조회", notes = "1RM 기록의 세부사항을 조회하는 API")
-    public ResponseEntity<PersonalRecordDto> getRecord(@PathVariable int personalRecordId) {
-        try {
-            PersonalRecordDto result = personalRecordService.getRecord(personalRecordId);
-            return new ResponseEntity<>(result, HttpStatus.OK);
-        } catch (Exception e) {
-            log.error(e.getMessage());
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
+	@PostMapping("/create")
+	@ApiOperation(value = "1RM 기록 생성", notes = "1RM 기록을 생성하는 API")
+	@ApiModelProperty(example = "{\"workoutName\":\"Squat\",\"maxWeight\":100}")
+	public ResponseEntity<Boolean> createRecord(@RequestHeader String Authorization,
+		@RequestBody PersonalRecordCreateRequest requestBody) {
+		try {
+			Optional<Integer> UID = jwtService.extractUID(Authorization);
+			boolean result = personalRecordService.creatRecord(UID.get(), requestBody);
+			return new ResponseEntity<>(result, HttpStatus.OK);
+		} catch (Exception e) {
+			log.error(e.getMessage());
+			return new ResponseEntity<>(false, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 
-    @PostMapping("/create")
-    @ApiOperation(value = "1RM 기록 생성", notes = "1RM 기록을 생성하는 API")
-    @ApiModelProperty(example = "{\"workoutName\":\"Squat\",\"maxWeight\":100}")
-    public ResponseEntity<Boolean> createRecord(@RequestHeader String Authorization,
-                                                @RequestBody PersonalRecordCreateRequest requestBody) {
-        try {
-            Optional<Integer> UID = jwtService.extractUID(Authorization);
-            boolean result = personalRecordService.creatRecord(UID.get(), requestBody);
-            return new ResponseEntity<>(result, HttpStatus.OK);
-        } catch (Exception e) {
-            log.error(e.getMessage());
-            return new ResponseEntity<>(false, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
+	@PutMapping("/modify/{personalRecordId}")
+	@ApiOperation(value = "1RM 기록 수정", notes = "1RM 기록을 수정하는 API")
+	public ResponseEntity<Boolean> modifyRecord(@PathVariable int personalRecordId,
+		@RequestHeader String Authorization, @RequestBody HashMap<String, Integer> requestBody) {
+		try {
+			Optional<Integer> UID = jwtService.extractUID(Authorization);
+			boolean result = personalRecordService.modifyRecord(UID.get(), requestBody, personalRecordId);
+			return new ResponseEntity<>(result, HttpStatus.OK);
+		} catch (Exception e) {
+			log.error(e.getMessage());
+			return new ResponseEntity<>(false, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 
-    @PutMapping("/modify/{personalRecordId}")
-    @ApiOperation(value = "1RM 기록 수정", notes = "1RM 기록을 수정하는 API")
-    public ResponseEntity<Boolean> modifyRecord(@PathVariable int personalRecordId,
-                                                @RequestHeader String Authorization, @RequestBody HashMap<String, Integer> requestBody) {
-        try {
-            Optional<Integer> UID = jwtService.extractUID(Authorization);
-            boolean result = personalRecordService.modifyRecord(UID.get(), requestBody, personalRecordId);
-            return new ResponseEntity<>(result, HttpStatus.OK);
-        } catch (Exception e) {
-            log.error(e.getMessage());
-            return new ResponseEntity<>(false, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    @DeleteMapping("/delete/{personalRecordId}")
-    @ApiOperation(value = "1RM 기록 삭제", notes = "1RM 기록을 삭제하는 API")
-    public ResponseEntity<Boolean> deleteRecord(@PathVariable int personalRecordId,
-                                                @RequestHeader String Authorization) {
-        try {
-            Optional<Integer> UID = jwtService.extractUID(Authorization);
-            return new ResponseEntity<>(personalRecordService.deleteRecord(personalRecordId, UID.get()), HttpStatus.OK);
-        } catch (Exception e) {
-            log.error(e.getMessage());
-            return new ResponseEntity<>(false, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
+	@DeleteMapping("/delete/{personalRecordId}")
+	@ApiOperation(value = "1RM 기록 삭제", notes = "1RM 기록을 삭제하는 API")
+	public ResponseEntity<Boolean> deleteRecord(@PathVariable int personalRecordId,
+		@RequestHeader String Authorization) {
+		try {
+			Optional<Integer> UID = jwtService.extractUID(Authorization);
+			return new ResponseEntity<>(personalRecordService.deleteRecord(personalRecordId, UID.get()), HttpStatus.OK);
+		} catch (Exception e) {
+			log.error(e.getMessage());
+			return new ResponseEntity<>(false, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 }
--- fitter/backend/src/main/java/com/mk/fitter/api/personalrecord/repository/PersonalRecordRepository.java
+++ fitter/backend/src/main/java/com/mk/fitter/api/personalrecord/repository/PersonalRecordRepository.java
@@ -1,31 +1,33 @@
 package com.mk.fitter.api.personalrecord.repository;
 
-import com.mk.fitter.api.personalrecord.repository.dto.PersonalRecordDto;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Map;
+import com.mk.fitter.api.personalrecord.repository.dto.PersonalRecordDto;
 
 @Repository
 public interface PersonalRecordRepository extends JpaRepository<PersonalRecordDto, Integer> {
 
-    List<PersonalRecordDto> findByUserDto_Id(int userId);
+	List<PersonalRecordDto> findByUserDto_Id(int userId);
 
-    boolean deleteById(int id);
+	boolean deleteById(int id);
 
-    @Query(value = "SELECT workout.*, ranked_records.*\n" +
-            "FROM workout\n" +
-            "LEFT OUTER JOIN (\n" +
-            "    SELECT pr.id as pr_id, pr.max_weight, pr.workout_id, RANK() OVER (PARTITION BY pr.workout_id ORDER BY pr.max_weight DESC) AS ranking\n" +
-            "    FROM personal_record pr\n" +
-            "    WHERE pr.user_id = :userId\n" +
-            ") AS ranked_records ON workout.id = ranked_records.workout_id\n" +
-            "where ranking = 1 or ranking is null;", nativeQuery = true)
-    List<Map<String, String>> findRankByUserDto_Id(@Param("userId") int userId);
+	@Query(value = "SELECT workout.*, ranked_records.*\n" +
+		"FROM workout\n" +
+		"LEFT OUTER JOIN (\n" +
+		"    SELECT pr.id as pr_id, pr.max_weight, pr.workout_id, RANK() OVER (PARTITION BY pr.workout_id ORDER BY pr.max_weight DESC) AS ranking\n"
+		+
+		"    FROM personal_record pr\n" +
+		"    WHERE pr.user_id = :userId\n" +
+		") AS ranked_records ON workout.id = ranked_records.workout_id\n" +
+		"where ranking = 1 or ranking is null;", nativeQuery = true)
+	List<Map<String, String>> findRankByUserDto_Id(@Param("userId") int userId);
 
-    List<PersonalRecordDto> findByUserDto_IdAndWorkoutDto_Name(int userId, String workoutName);
+	List<PersonalRecordDto> findByUserDto_IdAndWorkoutDto_Name(int userId, String workoutName);
 
 }
