b5961f6564d5cff50925f470c1e3a9c9b20b9d2d
정진욱 <wlsdnr214@naver.com>
2023-10-01 22:20:14 +0900

    Feat: feature 소개 페이지 구현

--- fitter/frontend-web/fitter/src/components/tointroduce/FeatureComponent.jsx
+++ fitter/frontend-web/fitter/src/components/tointroduce/FeatureComponent.jsx
@@ -3,7 +3,16 @@ function FeatureComponent() {
         <div className="FeatureComponent">
             <h1>기능 소개 페이지</h1>
             <div>
-                이러한 기능들이 있습니다.
+                <img src="imgs/feature1.jpg" alt="feature1" />
+            </div>
+            <div>
+                <img src="imgs/feature2.jpg" alt="feature2" />
+            </div>
+            <div>
+                <img src="imgs/feature3.jpg" alt="feature3" />
+            </div>
+            <div>
+                <img src="imgs/feature4.jpg" alt="feature4" />
             </div>
         </div>
     )
