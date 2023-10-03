7a9a329a5d4242c958a6d5ec9fe3c781851b35ad
정진욱 <wlsdnr214@naver.com>
2023-10-03 18:18:54 +0900

    Feat: 컨트롤러 상태코드 추가

--- fitter/backend-data/news/src/main/java/com/kafka/news/controller/KafkaController.java
+++ fitter/backend-data/news/src/main/java/com/kafka/news/controller/KafkaController.java
@@ -1,5 +1,7 @@
 package com.kafka.news.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,13 +21,15 @@ public class KafkaController {
 
 	private final NewsWordProducer newsWordProducer;
 	@PostMapping("/test")
-	public void sendMessage(@RequestParam String message) {
+	public ResponseEntity<String> sendMessage(@RequestParam String message) {
 		keywordTestProducer.sendMessage(message);
+		return new ResponseEntity<>("메시지 보내기 실행 완료", HttpStatus.OK);
 	}
 
 	@PostMapping("/keyword")
-	public void sendNewsKeyword(){
+	public ResponseEntity<String> sendNewsKeyword(){
 		newsWordProducer.sendNewsKeyword();
+		return new ResponseEntity<>("뉴스 키워드 보내기 실행 완료", HttpStatus.OK);
 	}
 
 
