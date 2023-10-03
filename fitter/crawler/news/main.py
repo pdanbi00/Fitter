4fdd6afe9c7d81e1d9f905e20b4cac5cc6f24b78
정진욱 <wlsdnr214@naver.com>
2023-10-03 17:09:17 +0900

    Fix: 크롤링 방식 스케줄링에서 요청방식으로 변경

--- fitter/crawler/news/main.py
+++ fitter/crawler/news/main.py
@@ -46,12 +46,28 @@ def health_crawler(request: Request):
 
 @app.post("/api/sports")
 def sports_crawler():
-    start_sports_crawler()
+    try:
+        start_sports_crawler()
+        return {"message": "Sports crawler started successfully"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error occurred: {e}")
 
 
 @app.post("/api/health")
 def health_crawler():
-    start_health_crawler()
+    try:
+        start_health_crawler()
+        return {"message": "Health crawler started successfully"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error occurred: {e}")
+
+@app.post("/api/refresh")
+def refresh_files():
+    try:
+        delete_old_files()
+        return {"message": "delete old files successfully"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error occurred: {e}")
 
 
 def start_sports_crawler():
@@ -86,19 +102,19 @@ def delete_old_files():  # 일주일 지난 크롤링 파일 삭제
                 os.remove(file_path)
 
 
-def show_current_time():
-    current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    print("Scheduler executed at:", current_datetime)
-
-
-scheduler = BackgroundScheduler(timezone=timezone('Asia/Seoul'))
-scheduler.add_job(start_sports_crawler, 'cron', hour=0, minute=1)
-scheduler.add_job(start_health_crawler, 'cron', hour=0, minute=1)
-scheduler.add_job(show_current_time, 'cron', hour=0, minute=2)
-scheduler.add_job(delete_old_files, 'cron', hour=0, minute=10)
-scheduler.start()
-
-
-@atexit.register
-def shutdown_scheduler():
-    scheduler.shutdown()
+# def show_current_time():
+#     current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+#     print("Scheduler executed at:", current_datetime)
+#
+#
+# scheduler = BackgroundScheduler(timezone=timezone('Asia/Seoul'))
+# scheduler.add_job(start_sports_crawler, 'cron', hour=0, minute=1)
+# scheduler.add_job(start_health_crawler, 'cron', hour=0, minute=1)
+# scheduler.add_job(show_current_time, 'cron', hour=0, minute=2)
+# scheduler.add_job(delete_old_files, 'cron', hour=0, minute=10)
+# scheduler.start()
+#
+#
+# @atexit.register
+# def shutdown_scheduler():
+#     scheduler.shutdown()
