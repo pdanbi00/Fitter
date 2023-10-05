ec73212d52655ffb918035f7598cf096a2c63141
김경숙 <lovablekks@naver.com>
2023-10-05 15:41:01 +0900

    Fix: 랭킹 한 사람의 모든 기록이 뜨는 오류 수정

--- fitter/backend/src/main/java/com/mk/fitter/api/namedwod/repository/WodRecordRepository.java
+++ fitter/backend/src/main/java/com/mk/fitter/api/namedwod/repository/WodRecordRepository.java
@@ -24,7 +24,11 @@ public interface WodRecordRepository extends JpaRepository<WodRecordDto, Integer
 
 	List<WodRecordDto> findByUser_Id(int userId);
 
-	@Query(value = "SELECT *, RANK() over(ORDER BY time ASC) AS ranking FROM wod_record WHERE wod_id = :wodId", nativeQuery = true)
+	@Query(value =
+		"SELECT w.id, w.wod_id, w.user_id, w.time, w.count, w.create_date, RANK() OVER(ORDER BY time ASC) AS ranking FROM\n"
+			+ "( SELECT user_id, min(time) AS min_time FROM wod_record GROUP BY user_id)\n"
+			+ "AS r INNER JOIN wod_record AS w ON r.user_id = w.user_id AND r.min_time = w.time\n"
+			+ "WHERE wod_id = :wodId;", nativeQuery = true)
 	Page<WodRecordDto> findRankById(@Param("wodId") int wodId, Pageable pageable);
 
 	@Query(value = "SELECT * FROM (SELECT *, RANK() OVER(ORDER BY TIME ASC) AS ranking FROM wod_record WHERE wod_id = :wodId) wod_record WHERE user_id = :userId LIMIT 1", nativeQuery = true)
